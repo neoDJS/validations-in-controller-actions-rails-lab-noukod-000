cbe30e5666a9b59bed149d1155a08fde4c5c3c20
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-18 19:08:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validate :validCategory
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+
+  def validCategory
+    ["Fiction", "Non-Fiction"].include?(category)
+  end
 end
